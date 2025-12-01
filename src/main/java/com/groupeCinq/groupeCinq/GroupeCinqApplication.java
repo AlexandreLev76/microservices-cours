b33cbc2cc96ba5c03e2b0503a0dd21909bd93f1a
alexandrelevenez <alexandre.levenez@ecoles-epsi.net>
2025-12-01 17:39:32 +0100

    Ajout de la structure du projet

---
+++ src/main/java/com/groupeCinq/groupeCinq/GroupeCinqApplication.java
@@ -0,0 +1,13 @@
+package com.groupeCinq.groupeCinq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupeCinqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupeCinqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/groupeCinq/groupeCinq/GroupeCinqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groupeCinq.groupeCinq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupeCinqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
